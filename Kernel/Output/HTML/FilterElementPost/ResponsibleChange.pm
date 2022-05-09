2895ea6ae3a14ac771f641fc58ebb7efff7b6aa4
reneeb <info@perl-services.de>
2022-05-09 11:10:11 +0200

    Ran bump-copyright-year.pl

--- Kernel/Output/HTML/FilterElementPost/ResponsibleChange.pm
+++ Kernel/Output/HTML/FilterElementPost/ResponsibleChange.pm
@@ -1,6 +1,6 @@
 # --
 # Kernel/Output/HTML/FilterElementPost/ResponsibleChange.pm
-# Copyright (C) 2016 Perl-Services.de, http://www.perl-services.de/
+# Copyright (C) 2016 - 2022 Perl-Services.de, https://www.perl-services.de/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
